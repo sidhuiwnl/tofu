083d10de893113a2ffd8ec9dd1536322f28aa599
Sidharth <sidharthinfernal@gmail.com>
2024-09-01 13:26:41 +0530

    init

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,33 +1,24 @@
+import WebSocket from "ws";
 
-import WebSocket from "ws"
+const wss = new WebSocket.Server({ port: 8000 });
 
-const wss = new WebSocket.Server({port : 8000});
+wss.on("connection", (ws) => {
+  console.log("a client connected");
 
-wss.on('connection',(ws) =>{
-    console.log("a client connected");
+  ws.on("message", (message: WebSocket.RawData) => {
+    const data = JSON.parse(message.toString());
 
-    ws.on('message',(message : WebSocket.RawData) =>{
-        const data = JSON.parse(message.toString());
-        
-        // Broadcast the message to all clients except the sender
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(data));
-
-            }
-        });
-
-        
-
-
-
-    })
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
+   
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
     });
-})
+  });
 
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
 
-
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log("WebSocket server is running on ws://localhost:8080");
